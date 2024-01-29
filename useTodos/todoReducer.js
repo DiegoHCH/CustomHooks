d2f39dae6779edef8ee1f7637db8ad638084a48d
Diego Hoyos <diego.hoyos@wiedii.co>
2024-01-29 14:52:08 -0500

    useReducer add

---
+++ useTodos/todoReducer.js
@@ -0,0 +1,29 @@
+
+
+export const todoReducer = ( initialState, action, completed ) => {
+
+    switch ( action.type ) {
+        case 'Add Todo':
+            return [ ...initialState, action.payload  ];
+        
+        case 'Delete Todo':
+            return initialState.filter( todo => todo.id !== action.payload );
+
+        case 'Complete Todo':
+            return initialState.map( todo => {
+
+                if ( todo.id === action.payload ) {
+                    return {
+                        ...todo,
+                        done: !todo.done
+                    }
+                }
+
+                return todo;
+            });
+
+        default:
+            return initialState;
+    }
+}
+
